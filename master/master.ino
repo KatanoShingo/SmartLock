eb0ce3fbaa35fd8b90cdf35b3901cfcd88c4878f
shingo <40855834+KatanoShingo@users.noreply.github.com>
2022-01-25 21:01:04 +0900

    Blynkでスマホで操作出来るように対応

--- master/master.ino
+++ master/master.ino
@@ -1,5 +1,8 @@
-b#include <nRF24L01.h>
+#include <nRF24L01.h>
 #include <RF24.h>
+#include <BlynkSimpleEsp32_BLE.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
 #define button 0                        
 int request;
 RTC_DATA_ATTR int keyStatus;
@@ -7,6 +10,7 @@ RTC_DATA_ATTR int keyStatus;
 RF24 radio(4, 22);               // CE,CSNピンの指定
 const byte address[6] = "00001";  // データを受信するアドレス
 
+char auth[] = "YourAuthToken";
 void setup()
 {
   // シリアルポートの初期化
@@ -19,10 +23,15 @@ void setup()
   radio.setPALevel(RF24_PA_MIN);  // 出力を最小に
   radio.stopListening();          // 送信側として設定
   
+  Blynk.setDeviceName("Blynk");
+
+  Blynk.begin(auth);
+  Blynk.run();
 }
 
 void loop()
 {      
+  Blynk.run();
   if(digitalRead(button)==0)
   {
     radio.write(&request, sizeof(request)); 
